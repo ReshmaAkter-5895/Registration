384aee0d7df13f6275b038c52100d38179014934
ReshmaAkter-5895 <59836488+ReshmaAkter-5895@users.noreply.github.com>
2022-03-23 17:12:48 +0600

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Registration_Form
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string gender;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        protected void btnReg_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=register;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO[dbo].[register]
+           ([name]
+           ,[address]
+           ,[gender]
+           ,[phone]
+           ,[email]
+           ,[username]
+           ,[password])
+     VALUES
+           ('" + txtName.Text + "', '" + txtadd.Text + "','" + gender + "','" + txtphone.Text + "','" + txtEmail.Text + "','" + txtuser.Text + "','" + txtpass.Text + "')", con);
+           con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('user is registered successfully')</script>");
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "male";
+        }
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "female";
+        }
+
+        protected void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "others";
+        }
+    }
+}
